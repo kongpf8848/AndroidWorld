838afb734164971d77692980eef765739a357127
kongpengfei <kongpf8848@gmail.com>
2021-04-20 15:12:28 +0800

    更新代码

--- app/src/main/java/com/github/kongpf8848/androidworld/utils/FlingBehavior.kt
+++ app/src/main/java/com/github/kongpf8848/androidworld/utils/FlingBehavior.kt
@@ -2,6 +2,7 @@ package com.github.kongpf8848.androidworld.utils
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.recyclerview.widget.RecyclerView
@@ -12,18 +13,22 @@ class FlingBehavior @JvmOverloads constructor(
     attrs: AttributeSet?=null
 ) : AppBarLayout.Behavior(context, attrs) {
 
-    private var a = false
+    private val TOP_CHILD_FLING_THRESHOLD = 3
+    private var isPositive = false
 
     override fun onNestedFling(coordinatorLayout: CoordinatorLayout, child: AppBarLayout, target: View, velocityX: Float, velocityY: Float, consumed: Boolean): Boolean {
 
+        Log.d("JACK8",
+            "onNestedFling() called with: coordinatorLayout = $coordinatorLayout, child = $child, target = $target, velocityX = $velocityX, velocityY = $velocityY, consumed = $consumed"
+        )
         var velocityY=velocityY
         var consumed=consumed
 
-        if (velocityY > 0.0f && !a || velocityY < 0.0f && a) {
+        if (velocityY > 0.0f && !isPositive || velocityY < 0.0f && isPositive) {
             velocityY *= -1.0f
         }
         if (target is RecyclerView && velocityY < 0.0f) {
-            consumed = target.getChildAdapterPosition(target.getChildAt(0)) > 3
+            consumed = target.getChildAdapterPosition(target.getChildAt(0)) > TOP_CHILD_FLING_THRESHOLD
         }
         return super.onNestedFling(
             coordinatorLayout,
@@ -37,6 +42,10 @@ class FlingBehavior @JvmOverloads constructor(
 
     override fun onNestedPreScroll(coordinatorLayout: CoordinatorLayout, child: AppBarLayout, target: View, dx: Int, dy: Int, consumed: IntArray) {
         super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed)
-        a = dy > 0
+        Log.d(
+            "JACK8",
+            "onNestedPreScroll() called with: coordinatorLayout = $coordinatorLayout, child = $child, target = $target, dx = $dx, dy = $dy, consumed = $consumed"
+        )
+        isPositive = dy > 0
     }
 }
